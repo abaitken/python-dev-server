3767533f279c0f69cd60b5c66be961360a2d1a4e
Alex Boyne-Aitken <abaitken@live.co.uk>
2025-02-12 18:31:45 +0000

    feat: Implemented as multi threaded server

--- localserver/__main__.py
+++ localserver/__main__.py
@@ -1,16 +1,22 @@
 import os
 from http.server import HTTPServer
+import socketserver
+import threading
 import CGIHandlerFactory, CustomCGIRequestHandler, Logging
+
+class ThreadedHTTPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
 
 if __name__ == '__main__':
     Logging.Level = Logging.LEVEL_INFO
     CGIHandlerFactory.CreateHandlers()
     serverport = os.getenv('SERVERPORT', '8080')
+    serveraddr = os.getenv('SERVERADDR', '')
     Logging.Log(Logging.LEVEL_INFO, 'serverport=' + serverport)
 
     try:
-        httpd = HTTPServer(('', int(serverport)), 
-                           CustomCGIRequestHandler.CustomCGIRequestHandler)
+        httpd = ThreadedHTTPServer((serveraddr, int(serverport)),
+                                   CustomCGIRequestHandler.CustomCGIRequestHandler)
 
         print(f"Running server. Use [ctrl]-c to terminate.")
 
